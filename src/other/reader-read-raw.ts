9959ca4527c80327c8cdc8fccd1f14f9d95e475e
Hugo Josefson <hugo@josefson.org>
2023-04-25 15:44:00 +0200

    refactor: move other examples out of the way

--- src/other/reader-read-raw.ts
+++ src/other/reader-read-raw.ts
@@ -1,6 +1,8 @@
-import logger from "./logger.ts";
+import logger from "../logger.ts";
 
 const buffer: Uint8Array = new Uint8Array(1024);
+Deno.stdin.setRaw(true);
 const numberOfBytes: number = <number> await Deno.stdin.read(buffer);
+Deno.stdin.setRaw(false);
 const bytes: Uint8Array = buffer.subarray(0, numberOfBytes);
 logger.info(`Reader.read -> ${numberOfBytes} bytes: ${bytes}`);
